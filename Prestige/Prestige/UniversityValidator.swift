8bc3664be060c3e718d22862213fcb68883c24f7
aaditprayag <aaditprayag@gmail.com>
2025-06-01 11:51:34 -0700

    added GT + NYU

--- Prestige/Prestige/UniversityValidator.swift
+++ Prestige/Prestige/UniversityValidator.swift
@@ -20,7 +20,7 @@ class UniversityValidator {
         University(name: "Columbia University", domains: ["columbia.edu"], rank: 7),
         University(name: "University of Pennsylvania", domains: ["upenn.edu"], rank: 8),
         University(name: "Johns Hopkins University", domains: ["jhu.edu"], rank: 9),
-        University(name: "Northwestern University", domains: ["northwestern.edu"], rank: 10),
+        University(name: "Northwestern University", domains: ["u.northwestern.edu"], rank: 10),
         University(name: "Duke University", domains: ["duke.edu"], rank: 11),
         University(name: "Dartmouth College", domains: ["dartmouth.edu"], rank: 12),
         University(name: "Brown University", domains: ["brown.edu"], rank: 13),
@@ -35,7 +35,9 @@ class UniversityValidator {
         University(name: "University of Michigan", domains: ["umich.edu"], rank: 22),
         University(name: "Georgetown University", domains: ["georgetown.edu"], rank: 23),
         University(name: "University of Virginia", domains: ["virginia.edu"], rank: 24),
-        University(name: "Washington University in St. Louis", domains: ["wustl.edu"], rank: 25)
+        University(name: "Washington University in St. Louis", domains: ["wustl.edu"], rank: 25),
+        University(name: "New York University", domains: ["stern.nyu.edu", "nyu.edu"], rank: 30),
+        University(name: "Georgia Institute of Technology", domains: ["gatech.edu"], rank: 33)
     ]
     
     func isValidUniversityEmail(_ email: String) -> (isValid: Bool, universityName: String?) {
